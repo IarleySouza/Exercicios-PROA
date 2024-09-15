022fdd7e7a7348de6a573aa9609f1a91afa46a54
iarley <iarleysouza615@gmail.com>
2024-09-14 21:00:17 -0300

    Atualizado

--- PROPROFISSÂO/js/Lista-Exercicio/ex04/script.js
+++ PROPROFISSÂO/js/Lista-Exercicio/ex04/script.js
@@ -1,37 +1,19 @@
-let A = parseFloat(prompt('Digite o valor de A:'))
-let B = parseFloat(prompt('Digite o valor de B:'))
-let C = parseFloat(prompt('Digite o valor de C:'))
+let A = parseFloat(prompt('Digite o valor de A:')) //88
+let B = parseFloat(prompt('Digite o valor de B:')) //99
+let C = parseFloat(prompt('Digite o valor de C:')) //100
 
-let maior, medio, menor;
-
-if (A >= B && A >= C) {
-    maior = A;
-    if (B >= C) {
-        medio = B;
-        menor = C;
-    } else {
-        medio = C;
-        menor = B;
-    }
-} else if (B >= A && B >= C) {
-    maior = B;
-    if (A >= C) {
-        medio = A;
-        menor = C;
-    } else {
-        medio = C;
-        menor = A;
-    }
-} else {
-    maior = C;
-    if (A >= B) {
-        medio = A;
-        menor = B;
-    } else {
-        medio = B;
-        menor = A;
-    }
+if (A > B && C < B){
+    document.write("O número maior é " + A + " e depois " + B + " e " + C)
+}else if (B > C && C > A){
+    document.write("O número maior é " + B + " e depois " + C + " e " + A)
+}else if(C > A && A > B){
+    document.write("O número maior é " + C + " e depois " + A + " e " + B)
+}else if(A > C && C > B){
+    document.write("O número maior é " + A + " e depois " + C + " e " + B)
+}else if(A < B && A > C){
+    document.write("O número maior é " + B + " e depois " + A + " e " + C)
+}else if(C > A && B > A){
+    document.write("O número maior é " + C + " e depois " + B + " e " + A)
+}else{
+    document.write("valor invalido")
 }
-document.write(` ${maior}<br>`)
-document.write(` ${medio}<br>`)
-document.write(` ${menor}`)
